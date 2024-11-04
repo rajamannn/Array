fbbefb8e1bfd9656e2b064026ab5e51ca7c9087a
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-05 00:47:30 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,18 @@
+//Program to print the array in the C++
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    arr[0] = 5;
+    arr[2] = -10;
+
+    arr[3 / 2] = 2;
+
+    arr[3] = arr[0];
+
+    cout << arr[0] << " " << arr[1] << " " << arr[2] << " "<< arr[3];
+
+    return 0;
+}
